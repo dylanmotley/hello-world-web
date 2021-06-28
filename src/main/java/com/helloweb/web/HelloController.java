29d51750d9f3a939b0ea76968d4bdc965fa43c25
unknown <motley.dylan60@gmail.com>
2021-06-27 20:52:06 -0400

    First Commit

---
+++ src/main/java/com/helloweb/web/HelloController.java
@@ -0,0 +1,49 @@
+package com.helloweb.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/hellos")
+public class HelloController {
+	
+	private List<String> names = new ArrayList<>();
+
+	// basic 'get' mapping - no parameter
+	@GetMapping("/")
+	public String printHello() {
+		return "Hello World";
+	}
+
+	// 'get' mapping accept a RequestParm
+	@GetMapping("/get")
+	public String printHello(@RequestParam String name) {
+		if (name == null || name.equals("")) {
+			name = "World";
+		}
+		return "Hello "+name+"!";
+	}
+	
+	// 'get' mapping accept a PathVariable
+	@GetMapping("/get/{id}")
+	public String printHello(@PathVariable int id) {
+		return "Hello "+id+"!";
+	}
+	
+	@GetMapping("/list")
+	public List<String> listNames() {
+		return names;
+	}
+	
+	// post mapping add a name to the list
+	@PostMapping("")
+	public String addName(@RequestParam String name) {
+		if (name != null && !name.equals("")) {
+			names.add(name);
+		}
+		return name+" added!";
+	}
+}
